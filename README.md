993330562b78b75e42c849a3796ad6eff4ef8d3e
sebastian jonasson <sebastianjonasson@gmail.com>
2016-09-06 20:58:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Victor Quotes CLI
+A Command-line tool for printing random quotes stated by Victor.
+
+### Installation
+Install the package globally
+
+    $ npm install -g victor-quotes-cli
+    
+### Usage
+Simply use the `victorq` command for printing a quote:
+
+    $ victorq
+     --> Victor states: [Quote]
---
+++ index.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+var quotes = [
+  '10000 öl',
+  'Man är ju inte nyponfärsk',
+  'Man känner sig ju inte som en nyplockad sommarblomma',
+  'Björna',
+  'Kasslergratäng',
+  'Nä, jag kör',
+  'Man är ju inte 20 längre'
+];
+
+var verbs = [
+	'states',
+	'declares',
+	'farts',
+	'sings'
+]
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomListValue() {
+  var index = getRandomInt(0, this.length-1);
+  return this[index];
+}
+
+
+var output = ` --> Victor ${getRandomListValue.call(verbs)}: ${getRandomListValue.call(quotes)}`;
+console.log(output);
